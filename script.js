89def01afa1069f4c6277e9ba44594b4b17055c6
Ryan Dyke <ryanjdyke@gmail.com>
2023-02-10 15:31:53 -0800

    Add UI and basic operations

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let displayValue = null;
+
+
+const numberButtons = document.querySelectorAll(".numberButton");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const backspaceButton = document.querySelector("#backspace");
+const topAnswerDisplay = document.querySelector("#topAnswerDisplay")
+const bottomAnswerDisplay = document.querySelector("#bottomAnswerDisplay")
+
+equalsButton.addEventListener("click", () => evaluate());
+
+numberButtons.forEach((button) => 
+  button.addEventListener("click", () => addToDisplay(button.textContent))
+)
+
+operatorButtons.forEach((button) => 
+  button.addEventListener("click", () => setOperation(button.textContent))
+)
+
+function addToDisplay(number) {
+  bottomAnswerDisplay.textContent += number;
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = bottomAnswerDisplay.textContent;
+  currentOperation = operator;
+  topAnswerDisplay.textContent = `${firstOperand} ${currentOperation}`;
+  clearBottomAnswerDisplay();
+}
+
+function clearBottomAnswerDisplay() {
+  bottomAnswerDisplay.textContent = "";
+}
+
+function evaluate() {
+  if (currentOperation === null) return;
+  secondOperand = bottomAnswerDisplay.textContent;
+  topAnswerDisplay.textContent += secondOperand;
+  bottomAnswerDisplay.textContent = operate(currentOperation, firstOperand, secondOperand);
+
+}
+
 function add(a, b) {
 	return a + b;
 };
@@ -19,10 +67,20 @@ function power(a, b) {
 };
 
 function operate(operator, num1, num2) {
-    if (operator === "+") return add(num1, num2);
-    if (operator === "-") return subtract(num1, num2);
-    if (operator === "*") return multiply(num1, num2);
-    if (operator === "/") return divide(num1, num2);
-    if (operator === "**") return power(num1, num2);
-    else return console.log("Operation Error")
+  num1 = Number(num1);
+  num2 = Number(num2);
+  switch(operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "x":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    case "**":
+      return power(num1, num2);
+    default:
+      return console.log("Operation Error");
+  }
 }
